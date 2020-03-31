60aebc8ec21ae56be0b0fa9cbfaae6874a78c12c
Coding Queen <tnp421@yahoo.com>
2020-03-31 01:30:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,13 +24,13 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age > 12 && age < 20) {
-    case age === 13: return 'You are a teenager'
-    case age === 14: return 'You are a teenager'
-    case age === 15: return 'You are a teenager'
-    case age === 16: return 'You are a teenager'
-    case age === 17: return 'You are a teenager'
-    case age === 18: return 'You are a teenager'
-    case age === 19: return 'You are a teenager'
+    case 13: return 'You are a teenager'
+    case 14: return 'You are a teenager'
+    case 15: return 'You are a teenager'
+    case 16: return 'You are a teenager'
+    case 17: return 'You are a teenager'
+    case 18: return 'You are a teenager'
+    case 19: return 'You are a teenager'
     default: return 'You have an age'
   }
 
